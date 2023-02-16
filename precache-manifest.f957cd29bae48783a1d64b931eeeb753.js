0304fad260a834001c4f8e67b35a6b7a0c666048
Code Sandbox <hello@codesandbox.io>
2023-02-16 12:40:03 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.f957cd29bae48783a1d64b931eeeb753.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0912bb59d050bc7352585c8c5baaecf",
+    "url": "https://mahekkumarchaniyara.github.io/csb-bmvbdv/index.html"
+  },
+  {
+    "revision": "f624104136581ad16af3",
+    "url": "https://mahekkumarchaniyara.github.io/csb-bmvbdv/static/css/main.462b0bd9.chunk.css"
+  },
+  {
+    "revision": "ea7bb9c1fa05866b3134",
+    "url": "https://mahekkumarchaniyara.github.io/csb-bmvbdv/static/js/2.04cd8e1a.chunk.js"
+  },
+  {
+    "revision": "f624104136581ad16af3",
+    "url": "https://mahekkumarchaniyara.github.io/csb-bmvbdv/static/js/main.9a4f3f6c.chunk.js"
+  },
+  {
+    "revision": "5e52fa21a4f89f4180a4",
+    "url": "https://mahekkumarchaniyara.github.io/csb-bmvbdv/static/js/runtime~main.bf009270.js"
+  }
+]);
---
+++ static/js/main.9a4f3f6c.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{3:function(n,e,t){n.exports=t(9)},8:function(n,e,t){},9:function(n,e,t){"use strict";t.r(e);var o=t(0),r=t.n(o),c=t(2),a=t.n(c);t(8);var u=function(){var n="hello";return r.a.createElement("button",{onClick:void(n="new")},n)};a.a.render(r.a.createElement(u,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.9a4f3f6c.chunk.js.map
